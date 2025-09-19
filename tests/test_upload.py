2519de2f9afc987ec097dc3e2eb04b7134fd466e
evnchn <evanchan040511@gmail.com>
2025-09-19 10:01:50 +0000

    tests: handler to dump content early, stop trying to access the file outside of the handler

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -9,10 +9,11 @@ test_path2 = Path('tests/test_scene.py').resolve()
 
 def test_uploading_text_file(screen: Screen):
     results: list[events.UploadEventArguments] = []
+    results_bytes: list[bytes] = []
 
     @ui.page('/')
     def page():
-        ui.upload(on_upload=results.append, label='Test Title')
+        ui.upload(on_upload=lambda e: results.append(e) or results_bytes.append(e.content.read()), label='Test Title')
 
     screen.open('/')
     screen.should_contain('Test Title')
@@ -23,7 +24,7 @@ def test_uploading_text_file(screen: Screen):
     assert len(results) == 1
     assert results[0].name == test_path1.name
     assert results[0].type in {'text/x-python', 'text/x-python-script'}
-    assert results[0].content.read() == test_path1.read_bytes()
+    assert results_bytes[0] == test_path1.read_bytes()
 
 
 def test_two_upload_elements(screen: Screen):
@@ -107,10 +108,12 @@ def test_reset_upload(screen: Screen):
 
 def test_multi_upload_event(screen: Screen):
     results: list[events.MultiUploadEventArguments] = []
+    results_bytes: list[bytes] = []
 
     @ui.page('/')
     def page():
-        ui.upload(on_multi_upload=results.append, multiple=True)
+        ui.upload(on_multi_upload=lambda e: results.append(e) or results_bytes.extend(
+            [content.read() for content in e.contents]), multiple=True)
 
     screen.open('/')
     screen.find_by_class('q-uploader__input').send_keys(f'{test_path1}\n{test_path2}')
@@ -120,5 +123,4 @@ def test_multi_upload_event(screen: Screen):
 
     assert len(results) == 1
     assert results[0].names == [test_path1.name, test_path2.name]
-    assert results[0].contents[0].read() == test_path1.read_bytes()
-    assert results[0].contents[1].read() == test_path2.read_bytes()
+    assert results_bytes == [test_path1.read_bytes(), test_path2.read_bytes()]
